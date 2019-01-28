f549c12eb5fc855b7763c1b401bf99181cb2a8b3
anhtuanhsgs <anhtuanhsgs@gmail.com>
2019-01-29 03:08:21 +0900

    fix

--- HeartRL/A3C/environment.py
+++ HeartRL/A3C/environment.py
@@ -40,7 +40,7 @@ class State:
         return self.start[0] + self.size [0] / 2, self.start[1] + self.size [1] / 2
 
     def debug (self):
-        print 'id:', self.id, 'start:', self.start, 'size:', self.size, 'img_id:', self.img_id, 'target:', self.target, 'depth:', self.depth
+        print ('id:', self.id, 'start:', self.start, 'size:', self.size, 'img_id:', self.img_id, 'target:', self.target, 'depth:', self.depth)
 
 class EM_env:
     def __init__ (self, raw_list, lbl_list, env_conf, obs_format="CHW"):
