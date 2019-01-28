19c9d338d9d734134457c3bee15a84d872bf617c
anhtuanhsgs <anhtuanhsgs@gmail.com>
2019-01-29 03:04:09 +0900

    fix

---
+++ HeartRL/A3C/main.py
@@ -0,0 +1,252 @@
+from __future__ import print_function, division
+import os, sys, glob, time
+os.environ["OMP_NUM_THREADS"] = "1"
+import argparse
+import torch
+import torch.multiprocessing as mp
+from environment import *
+from utils import read_config
+from model import *
+from train import train
+from test import test
+from natsort import natsorted
+
+from Utils.img_aug_func import *
+from Utils.utils import *
+from skimage.measure import label
+from CorrectorModule.corrector_utils import *
+from shared_optim import SharedRMSprop, SharedAdam
+
+
+parser = argparse.ArgumentParser(description='A3C')
+parser.add_argument(
+    '--env',
+    default='EM_env',
+    metavar='ENV',
+    help='environment to train on (default: EM_env)')
+
+parser.add_argument(
+    '--lr',
+    type=float,
+    default=0.0001,
+    metavar='LR',
+    help='learning rate (default: 0.0001)')
+
+parser.add_argument(
+    '--gamma',
+    type=float,
+    default=1,
+    metavar='G',
+    help='discount factor for rewards (default: 1)')
+
+parser.add_argument(
+    '--tau',
+    type=float,
+    default=1.00,
+    metavar='T',
+    help='parameter for GAE (default: 1.00)')
+
+parser.add_argument(
+    '--seed',
+    type=int,
+    default=1,
+    metavar='S',
+    help='random seed (default: 1)')
+
+parser.add_argument(
+    '--workers',
+    type=int,
+    default=4,
+    metavar='W',
+    help='how many training processes to use (default: 32)')
+
+parser.add_argument(
+    '--num-steps',
+    type=int,
+    default=4,
+    metavar='NS',
+    help='number of forward steps in A3C (default: 20)')
+
+parser.add_argument(
+    '--max-episode-length',
+    type=int,
+    default=41234,
+    metavar='M',
+    help='maximum length of an episode (default: 10000)')
+
+parser.add_argument(
+    '--save-max',
+    default=True,
+    metavar='SM',
+    help='Save model on every test run high score matched or bested')
+
+parser.add_argument(
+    '--load', default=False, metavar='L', help='load a trained model')
+
+parser.add_argument(
+    '--optimizer',
+    default='Adam',
+    metavar='OPT',
+    help='shares optimizer choice of Adam or RMSprop')
+
+parser.add_argument(
+    '--load-model-dir',
+    default='trained_models/',
+    metavar='LMD',
+    help='folder to load trained models from')
+
+parser.add_argument(
+    '--save-model-dir',
+    default='trained_models/',
+    metavar='SMD',
+    help='folder to save trained models')
+
+parser.add_argument(
+    '--log-dir', default='logs/', metavar='LG', help='folder to save logs')
+
+parser.add_argument(
+    '--gpu-ids',
+    type=int,
+    default=-1,
+    nargs='+',
+    help='GPUs to use [-1 CPU only] (default: -1)')
+
+parser.add_argument(
+    '--env-gpu',
+    type=int,
+    default=0,
+    help='GPUs to use [-1 CPU only] (default: -1)')
+
+parser.add_argument(
+    '--amsgrad',
+    default=True,
+    metavar='AM',
+    help='Adam optimizer amsgrad parameter')
+
+parser.add_argument(
+    '--save-period',
+    type=int,
+    default=50,
+    metavar='SP',
+    help='Adam optimizer amsgrad parameter')
+
+parser.add_argument(
+    '--log-period',
+    type=int,
+    default=10,
+    metavar='LP',
+    help='Adam optimizer amsgrad parameter')
+
+parser.add_argument (
+    '--spliter',
+    default='FusionNet',
+    metavar='SPL',
+    choices=['FusionNet', 'Thres'])
+
+parser.add_argument (
+    '--merger',
+    default='FusionNet',
+    metavar='MER',
+    choices=['FusionNet', 'Thres'])
+
+parser.add_argument(
+    '--shared-optimizer',
+    default=False,
+    metavar='SO',
+    help='use an optimizer without shared statistics.')
+
+parser.add_argument (
+    '--hidden-feat',
+    type=int,
+    default=512,
+    metavar='HF')
+
+def setup_env_conf (args):
+    env_conf = {
+        "agent_out_shape": [1, 8, 8],
+        "num_feature": 1,
+        "num_action": 1 * 8 * 8,
+        "observation_shape": [1, 256, 256],
+        "local_wd_size": [32, 32],
+        "env_gpu": args.env_gpu
+    }
+    return env_conf
+
+def get_data (path, args):
+    base_path = '/home/Pearl/tuan/_Data/ml16-master/segmentation/data/'
+    raw_path = base_path + 'train/images/'
+    label_path = base_path + 'train/labels/'
+
+    raw_files = natsorted (glob.glob (raw_path + '*.png'))
+    lbl_files = natsorted (glob.glob (label_path + '*.png'))
+
+    raw_list = read_im (raw_files)
+    lbl_list = read_im (lbl_files)
+    i = 0
+    while i < len (raw_list):
+        if np.sum (lbl_list[i]) == 0:
+            del lbl_list [i]
+            del raw_list [i]
+        else:
+            i += 1
+
+    for i in range (len (raw_list)):
+        raw_list[i] = np.squeeze (raw_list[i][:,:,0])
+
+    return raw_list, lbl_list
+
+def setup_data (env_conf):
+    raw, gt_lbl = get_data (path='Data/train/', args=None)
+    return raw, gt_lbl
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    
+    torch.manual_seed(args.seed)
+    if args.gpu_ids == -1:
+        args.gpu_ids = [-1]
+    else:
+        torch.cuda.manual_seed(args.seed)
+        mp.set_start_method('spawn')
+    env_conf = setup_env_conf (args)
+    raw, gt_lbl = setup_data (env_conf)
+
+    # env =EM_env (raw, lbl, prob, env_conf, 'train', gt_lbl)
+    shared_model = A3Clstm (env_conf ["observation_shape"], 
+                        env_conf["num_action"], args.hidden_feat)
+
+    if args.load:
+        saved_state = torch.load(
+            '{0}{1}.dat'.format(args.load_model_dir, args.env),
+            map_location=lambda storage, loc: storage)
+        shared_model.load_state_dict(saved_state)
+    shared_model.share_memory()
+    
+    if args.shared_optimizer:
+        if args.optimizer == 'RMSprop':
+            optimizer = SharedRMSprop(shared_model.parameters(), lr=args.lr)
+        if args.optimizer == 'Adam':
+            optimizer = SharedAdam(
+                shared_model.parameters(), lr=args.lr, amsgrad=args.amsgrad)
+        optimizer.share_memory()
+    else:
+        optimizer = None
+
+    processes = []
+
+    p = mp.Process(target=test, args=(args, shared_model, env_conf, [raw, gt_lbl]))
+    p.start()
+    processes.append(p)
+    time.sleep(1)
+
+    for rank in range(0, args.workers):
+        p = mp.Process(
+            target=train, args=(rank, args, shared_model, optimizer, env_conf, [raw, gt_lbl]))
+        p.start()
+        processes.append(p)
+        time.sleep(1)
+
+    for p in processes:
+        time.sleep(0.1)
+        p.join()
+
---
+++ HeartRL/A3C/train.py
@@ -0,0 +1,160 @@
+from __future__ import division
+from setproctitle import setproctitle as ptitle
+import torch
+import torch.optim as optim
+from environment import *
+from utils import ensure_shared_grads
+from model import *
+from player_util import Agent
+from torch.autograd import Variable
+from Utils.Logger import Logger
+
+import numpy as np
+
+def train (rank, args, shared_model, optimizer, env_conf, datasets):
+    ptitle('Training Agent: {}'.format(rank))
+    print ('Start training agent: ', rank)
+    
+    if rank == 0:
+        logger = Logger (args.log_dir)
+        train_step = 0
+
+    gpu_id = args.gpu_ids[rank % len(args.gpu_ids)]
+    torch.manual_seed(args.seed + rank)
+    if gpu_id >= 0:
+        torch.cuda.manual_seed(args.seed + rank)
+    raw, gt_lbl = datasets
+    env = EM_env (raw, gt_lbl, env_conf)
+    if optimizer is None:
+        if args.optimizer == 'RMSprop':
+            optimizer = optim.RMSprop (shared_model.parameters (), lr=args.lr)
+        if args.optimizer == 'Adam':
+            optimizer = optim.Adam (shared_model.parameters (), lr=args.lr, amsgrad=args.amsgrad)
+
+        # env.seed (args.seed + rank)
+    player = Agent (None, env, args, None)
+    player.gpu_id = gpu_id
+    player.model = A3Clstm (env.observation_space.shape, env_conf["num_action"], args.hidden_feat)
+
+    player.state = player.env.reset ()
+    player.state = torch.from_numpy (player.state).float ()
+
+    if gpu_id >= 0:
+        with torch.cuda.device (gpu_id):
+            player.state = player.state.cuda ()
+            player.model = player.model.cuda ()
+    player.model.train ()
+
+    if rank == 0:
+        eps_reward = 0
+        pinned_eps_reward = 0
+        mean_log_prob = 0
+
+    while True:
+        if gpu_id >= 0:
+            with torch.cuda.device (gpu_id):
+                player.model.load_state_dict (shared_model.state_dict ())
+        else:
+            player.model.load_state_dict (shared_model.state_dict ())
+        
+        if player.done:
+            player.eps_len = 0
+            if rank == 0:
+                pinned_eps_reward = eps_reward
+                eps_reward = 0
+                mean_log_prob = 0
+            if gpu_id >= 0:
+                with torch.cuda.device(gpu_id):
+                    player.cx = Variable(torch.zeros(1, args.hidden_feat).cuda())
+                    player.hx = Variable(torch.zeros(1, args.hidden_feat).cuda())
+            else:
+                player.cx = Variable(torch.zeros(1, args.hidden_feat))
+                player.hx = Variable(torch.zeros(1, args.hidden_feat))
+        else:
+            player.cx = Variable(player.cx.data)
+            player.hx = Variable(player.hx.data)
+
+        for step in range(args.num_steps):
+            player.action_train()
+            if rank == 0:
+                eps_reward += player.reward
+                mean_log_prob += player.log_probs [-1] / env_conf ["T"]
+            if player.done:
+                break
+
+        if player.done:
+            state = player.env.reset ()
+            player.state = torch.from_numpy (state).float ()
+            if gpu_id >= 0:
+                with torch.cuda.device (gpu_id):
+                    player.state = player.state.cuda ()
+
+        R = torch.zeros (1, 1)
+        if not player.done:
+            value, _, _ = player.model((Variable(player.state.unsqueeze(0)),
+                                        (player.hx, player.cx)))
+            R = value.data
+
+        if gpu_id >= 0:
+            with torch.cuda.device (gpu_id):
+                R = R.cuda ()
+
+        player.values.append(Variable(R))
+        policy_loss = 0
+        value_loss = 0
+        gae = torch.zeros(1, 1)
+
+        if gpu_id >= 0:
+            with torch.cuda.device (gpu_id):
+                R = R.cuda ()
+
+        player.values.append (Variable (R))
+        policy_loss = 0
+        value_loss = 0
+        gae = torch.zeros (1, 1)
+        if gpu_id >= 0:
+            with torch.cuda.device (gpu_id):
+                gae = gae.cuda ()
+        R = Variable (R)
+        for i in reversed(range(len(player.rewards))):
+            R = args.gamma * R + player.rewards[i]
+            advantage = R - player.values[i]
+            # value_loss = value_loss + F.smooth_l1_loss (input=player.values[i], target=R)
+            value_loss = value_loss + advantage.pow (2)
+
+            # Generalized Advantage Estimataion
+            delta_t = player.rewards[i] + args.gamma * player.values[i + 1].data - player.values[i].data
+            gae = gae * args.gamma * args.tau + delta_t
+            policy_loss = policy_loss - player.log_probs[i] * Variable(gae) - 0.05 * player.entropies[i]
+            entropy = player.entropies [i]
+
+        player.model.zero_grad ()
+        sum_loss = (policy_loss + value_loss)
+        sum_loss.backward ()
+        ensure_shared_grads (player.model, shared_model, gpu=gpu_id >= 0)
+        optimizer.step ()
+        player.clear_actions ()
+
+        if rank == 0:
+            train_step += 1
+            if train_step % args.log_period == 0:
+                log_info = {
+                    'sum_loss': sum_loss, 
+                    'value_loss': value_loss, 
+                    'policy_loss': policy_loss, 
+                    'advanage': advantage,
+                    'entropy': entropy,
+                    'train eps reward': pinned_eps_reward,
+                    'mean log prob': mean_log_prob
+                }
+
+                for tag, value in log_info.items ():
+                    logger.scalar_summary (tag, value, train_step)
+
+
+
+
+
+
+
+
