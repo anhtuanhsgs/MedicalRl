788f729ce25cc1b201c53401483289d0c7a8cf71
anhtuanhsgs <anhtuanhsgs@gmail.com>
2019-01-31 02:06:08 +0900

    a3c Heart

--- ErrorCorrector/environment.py
+++ ErrorCorrector/environment.py
@@ -244,7 +244,7 @@ class EM_env (gym.Env):
         if (self.step_cnt >= self.T):
             # reward += self.old_score * 10
             reward = self.old_score
-            if (reward < 0.2):
+            if (reward < 0.3):
                 reward = 0
             done = True
         else:
