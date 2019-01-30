32876a24b02631a46ec349dd8188022680dddc91
anhtuanhsgs <anhtuanhsgs@gmail.com>
2019-01-31 02:25:58 +0900

    a3c Heart

--- ErrorCorrector/player_util.py
+++ ErrorCorrector/player_util.py
@@ -41,16 +41,14 @@ class Agent (object):
         if not use_max:
             action = prob.multinomial(1).data
             self.action = action.cpu().numpy() [0][0]
-            log_prob = log_prob.gather(1, Variable(action.unsqueeze (0)))
+            log_prob = log_prob.gather(1, Variable(action))
             state, self.reward, self.done, self.info = self.env.step(
                 action.cpu().numpy())
         else:
             with torch.no_grad():
                 action = prob.max (1)[1].data
-                print (action.unsqueeze (0).shape)
-                print (log_prob.shape)
                 self.action = action.cpu().numpy() [0]
-                log_prob = log_prob.gather(1, Variable(action))
+                log_prob = log_prob.gather(1, Variable(action.unsqueeze (0)))
                 state, self.reward, self.done, self.info = self.env.step(
                     self.action)
         if not use_max:
